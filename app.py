b21923ac5fca57b4f4748f1d27ff0148d2a8491b
Akunna Anyamkpa <66309302+Akunnadove@users.noreply.github.com>
2026-06-24 13:21:06 +0100

    Remove unused tab from the app interface

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ df["predicted_class"] = model.predict(X)
 # ============================================================================
 
 # Create tabs
-tab1, tab2, tab3, tab4 = st.tabs(["Classification", "Flags"])
+tab1, tab2 = st.tabs(["Classification", "Flags"])
 
 with tab1:
     st.subheader("Classification Results")
